a16a534b610ee7b8151ae2fe0502c1d7c826162d
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-09-20 07:13:28 -0500

    feat: The method that creates the JWT was created

--- Config/Utilities.cs
+++ Config/Utilities.cs
@@ -1,5 +1,9 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
+using JWT_ProductManager.Models;
+using Microsoft.IdentityModel.Tokens;
 
 namespace JWT_ProductManager.Config;
 public class Utilities
@@ -16,6 +20,40 @@ public class Utilities
             }
             return builder.ToString();
         }
+    }
+
+
+    public string GenerateJwtToken(User user)
+    {
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.NameIdentifier,user.Id.ToString()),
+            new Claim(ClaimTypes.Email,user.Email),
+        };
+
+        var jwtKey = Environment.GetEnvironmentVariable("JWT_KEY");
+        var jwtIssuer = Environment.GetEnvironmentVariable("JWT_ISSUER");
+        var jwtAudience = Environment.GetEnvironmentVariable("JWT_AUDIENCE");
+        var jwtExpiresIn = Environment.GetEnvironmentVariable("JWT_EXPIRES_IN");
+
+        // Validar que las variables existen
+        if (string.IsNullOrEmpty(jwtKey) || string.IsNullOrEmpty(jwtIssuer) || string.IsNullOrEmpty(jwtAudience))
+        {
+            throw new InvalidOperationException("JWT configuration values are missing.");
+        }
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey));
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: jwtIssuer,
+            audience: jwtAudience,
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(Convert.ToDouble(jwtExpiresIn)),
+            signingCredentials: credentials
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
     }
 
 
